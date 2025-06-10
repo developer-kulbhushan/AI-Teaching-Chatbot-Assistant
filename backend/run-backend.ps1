63c6b3be5409e7a3963445921feefcc65d4f4867
Kulbhushan <v-kulbhushan@microsoft.com>
2025-06-10 13:33:07 +0530

    Updated Automation of Starting Backend Server

--- backend/run-backend.ps1
+++ backend/run-backend.ps1
@@ -1,3 +1,6 @@
+Write-Host "Activating virtual environment..."
+& ".\backend-env\Scripts\Activate.ps1"
+
 Write-Host "Starting backend server..."
 # Ensure the virtual environment is activated before running this script
 uvicorn backend:app --host 0.0.0.0 --port 8000 --reload
